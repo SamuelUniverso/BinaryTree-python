6095f84d084897616f2c22b07ec6b29dd453cd88
SamuelUniverso <samuel.rockenbach@universo.univates.br>
2023-03-22 19:10:18 -0300

    Traverse & Search Tree

--- Node.py
+++ Node.py
@@ -18,6 +18,26 @@ class Node:
                 else:
                         self.data = data
 
+        def findval(self, lkpval):
+                if lkpval < self.data:
+                        if self.left is None:
+                                return str(lkpval) + " Not Found"
+                        return self.left.findval(lkpval)
+                elif lkpval > self.data:
+                        if self.right is None:
+                                return str(lkpval) + " Not Found"
+                        return self.right.findval(lkpval)
+                else:
+                        print(str(self.data) + ' is found')
+
+        def inorderTraversal(self, root):
+                res = []
+                if root:
+                        res = self.inorderTraversal(root.left)
+                        res.append(root.data)
+                        res = res + self.inorderTraversal(root.right)
+                return res
+
         def printTree(self):
                 if self.left:
                         self.left.printTree()
