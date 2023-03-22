7068061786b850d9570750a2f3a3a4ac0857d459
SamuelUniverso <samuel.rockenbach@universo.univates.br>
2023-03-22 19:20:03 -0300

    Main Script

--- main.py
+++ main.py
@@ -4,5 +4,8 @@ root = Node(12)
 root.insert(6)
 root.insert(14)
 root.insert(3)
-
-root.printTree()
+print(root.findval(7))
+print(root.findval(14))
+print(root.inorderTraversal(root))
+root.print_1d()
+root.print_2d(root);
