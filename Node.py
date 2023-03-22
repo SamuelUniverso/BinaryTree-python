8a91badbbfa5c59c8a739978434d93f50419be74
SamuelUniverso <samuel.rockenbach@universo.univates.br>
2023-03-22 19:14:45 -0300

    Print2D

--- Node.py
+++ Node.py
@@ -44,3 +44,44 @@ class Node:
                 print(self.data)
                 if self.right:
                         self.right.printTree()
+
+def print_2d(root):
+    if not root:
+        return []
+
+    height = get_height(root)
+    width = 2 ** height - 1
+
+    # Initialize the result array
+    res = [[' ' for _ in range(width)] for _ in range(height)]
+
+    # Fill the result array
+    fill_res(root, res, 0, 0, width - 1)
+
+    # Print the result
+    for row in res:
+        print(''.join(row))
+
+def fill_res(root, res, depth, left, right):
+    if not root:
+        return
+
+    mid = (left + right) // 2
+    res[depth][mid] = str(root.data)
+    fill_res(root.left, res, depth + 1, left, mid - 1)
+    fill_res(root.right, res, depth + 1, mid + 1, right)
+
+def get_height(root):
+    if not root:
+        return 0
+
+    left_height = get_height(root.left)
+    right_height = get_height(root.right)
+    return max(left_height, right_height) + 1
+
+
+root = Node(12)
+root.insert(6)
+root.insert(14)
+root.insert(3)
+print_2d(root);
