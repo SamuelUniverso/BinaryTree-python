cc9790e2bdee5672d8dcaec4843da7902b39e069
SamuelUniverso <samuel.rockenbach@universo.univates.br>
2023-03-29 20:10:35 -0300

    height script

---
+++ height.py
@@ -0,0 +1,13 @@
+from BinaryTree import BinaryTree
+
+tree = BinaryTree()
+
+tree.insert(5)
+tree.insert(3)
+tree.insert(7)
+tree.insert(1)
+tree.insert(4)
+tree.insert(6)
+tree.insert(8)
+
+print(tree.tree_height())
